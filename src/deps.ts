3d7f9f15d8dac2c48a95acde1496399a99bde998
Thor Galle <thorgalle@gmail.com>
2023-03-04 18:39:50 +0200

    fix: move imports to import map

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,7 @@
 // Auto-loads .env and exports it to Deno.env
 // see https://deno.land/std@0.177.0/dotenv/mod.ts
 // Seems to require process env access to all.
-import "https://deno.land/std@0.177.0/dotenv/load.ts";
+import "std/dotenv/load.ts";
 export { parse as parseYAML } from "std/encoding/yaml.ts";
 export { serve } from "std/http/server.ts";
 export {
