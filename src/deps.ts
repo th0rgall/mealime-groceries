6357f7315feb1f322ba0c485638cb8f130c0490c
Thor Galle <thorgalle@gmail.com>
2023-03-04 18:18:36 +0200

    fix: relative import warning on deno deploy

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,8 @@
 // Auto-loads .env and exports it to Deno.env
 // see https://deno.land/std@0.177.0/dotenv/mod.ts
 // Seems to require process env access to all.
-import "std/dotenv/load.ts";
+// deno-lint-ignore no-unused-vars
+import * as load from "std/dotenv/load.ts";
 export { parse as parseYAML } from "std/encoding/yaml.ts";
 export { serve } from "std/http/server.ts";
 export {
